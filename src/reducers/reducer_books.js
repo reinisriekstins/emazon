4eb50126c4f90f7a7a3f080579ac4804bb7a04f5
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-02-28 08:35:38 +0200

    created books reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', pages: 819 },
+    { title: 'Lord of the Rings', pages: 2310 },
+    { title: 'Code Complete', pages: 1389 },
+    { title: 'The Second Machine Age', pages: 318 },
+    { title: 'Design Patterns', pages: 568 }
+  ]
+}
